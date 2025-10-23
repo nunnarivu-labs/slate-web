3aa8648de7238adc8cc58739aeb4231376345112
johnson-abraham <johnsonabraham0812@gmail.com>
2025-10-23 20:08:22 +0530

    Revert "removed react-markdown and install mdxeditor"
    This reverts commit dfa89b4fe134e0a61b2373a748e8023bd8b7948e.

---
+++ src/components/markdown.tsx
@@ -0,0 +1,44 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+const markdownComponents = {
+  p: ({ node, ...props }: any) => {
+    return (
+      <p
+        className="text-sm break-words whitespace-pre-wrap text-zinc-700 dark:text-zinc-300"
+        {...props}
+      />
+    );
+  },
+
+  li: ({ node, ...props }: any) => {
+    return (
+      <li
+        className="text-sm break-words whitespace-pre-wrap text-zinc-700 dark:text-zinc-300"
+        {...props}
+      />
+    );
+  },
+
+  strong: ({ node, ...props }: any) => {
+    return (
+      <strong
+        className="text-sm break-words whitespace-pre-wrap text-zinc-700 dark:text-zinc-300"
+        {...props}
+      />
+    );
+  },
+};
+
+export const Markdown = ({ content }: { content: string }) => {
+  return (
+    <div style={{ whiteSpace: 'pre-wrap' }}>
+      <ReactMarkdown
+        components={markdownComponents}
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+};
