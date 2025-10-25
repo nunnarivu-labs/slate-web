7c986a7ffc562dc2b22b69802430ed9a7638bcc6
johnson-abraham <johnsonabraham0812@gmail.com>
2025-10-25 14:57:04 +0530

    moved notes fetching inside the component.

--- src/routes/_auth/notes/$category/route.tsx
+++ src/routes/_auth/notes/$category/route.tsx
@@ -1,22 +1,14 @@
-import { Loader } from '@/components/loader.tsx';
 import { NotesApp } from '@/components/notes-app.tsx';
 import { NoteCategory } from '@/types/note-category.ts';
-import { docToNote } from '@/utils/doc-note-converter.ts';
 import { isValidNoteCategory } from '@/utils/note-categoty-params.ts';
-import { convexQuery } from '@convex-dev/react-query';
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
 
-import { api } from '../../../../../convex/_generated/api';
-
-const NotesRoute = () => {
-  console.log('Inside Notes Route');
-  return (
-    <>
-      <NotesApp />
-      <Outlet />
-    </>
-  );
-};
+const NotesRoute = () => (
+  <>
+    <NotesApp />
+    <Outlet />
+  </>
+);
 
 export const Route = createFileRoute('/_auth/notes/$category')({
   params: {
@@ -35,13 +27,4 @@ export const Route = createFileRoute('/_auth/notes/$category')({
   },
 
   component: NotesRoute,
-
-  pendingComponent: () => <Loader />,
-
-  loader: async ({ context: { queryClient }, params: { category } }) =>
-    (
-      await queryClient.fetchQuery({
-        ...convexQuery(api.tasks.fetchNotes, { category }),
-      })
-    ).map(docToNote),
 });
