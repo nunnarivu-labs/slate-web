fa93010b376c527725a8fe72a9d78d7139e8f7d1
johnson-abraham <johnsonabraham0812@gmail.com>
2025-10-29 09:39:15 +0530

    react-markdown integration

--- src/components/markdown/markdown-preview.tsx
+++
@@ -1,7 +0,0 @@
-type MarkdownPreviewProps = {
-  md: string;
-};
-
-export const MarkdownPreview = ({ md }: MarkdownPreviewProps) => (
-  <p className="text-zinc-800 dark:text-zinc-200">{md}</p>
-);
--- src/components/markdown/markdown-utils.tsx
+++
@@ -1,8 +0,0 @@
-export const components = {
-  ul: ({ node, ...props }: any) => {
-    return <ul className="list-disc" {...props} />;
-  },
-  ol: ({ node, ...props }: any) => {
-    return <ul className="list-decimal" {...props} />;
-  },
-};
